57f9e0f1a8dc962aad7d8ffff4e7e2be41743c50
Arelia <arelia@github.com>
2019-09-15 02:09:52 -0500

    add letter

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,51 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>July 23 – July 26, 2020</p>
-    <p>Los Angeles, California</p>
-    <p>Get ready for family fun in the sun L.A. style. We are planning a great time for all. Our tentative itinerary for this great 4-day event is as follows.</p>
-    <ul>
-      <li><strong>Thursday</strong>: 	Meet and Greet reception at hotel. This will include registration check-in, a light meal and fun and games for all.  Followed by free time for local extravaganzas at nearby attractions.</li>
-      <li><strong>Friday</strong>:	Casino Bus Trip for adults.  Amusement Park Getaway or Bowling for youth.</li>
-      <li><strong>Saturday</strong>:	Picnic at local park from 10:00 – 2:00</li>
-      <li><strong>Sunday</strong>:	Sunday Morning Worship at New Commandment Baptist Church, followed by Banquet Dinner at Hotel.  </li>
-    </ul>
-    <hr></hr>
+    <div>
+      <h1>Jones Family Reunion<br></br>2020 VISIONARY</h1>
 
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSchPXBvLJtD2s_nmR13GkNwBv90CQ167G0B7UexxbU3kR6euA/viewform?embedded=true" width="640" height="6349" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <p>Hey family it’s time to get it popping out here. Time for the Jones Family to once again come together to celebrate Family, Unity and Strength! <strong>When: July 23 – 26, 2020</strong>. It is hard to believe that it has been nearly 2 years since the last reunion. Well this year we are meeting in the City of Angels. That’s right, we are coming to <strong>Los Angeles, California</strong>. If you have never been here, get your ticket now. Get on the bus, plane, train or in the car where all roads will lead to Los Angeles.</p>
 
+      <p>This year’s reunion committee has been hard at work trying to plan and prepare for your arrival. We want to see everyone from near and far. Those cousins who live on the other coast, mid-west or wherever it is that you reside, let’s make this happen. We only do this once every 2 years.  Don’t let this opportunity go and miss spending time with your family. We love you! We will have time to spend hearing about old family legends and stories, exchanging recipes, finding out who’s who and maybe even discovering unknown talents. There are so many wonderful things to be explored within our own family. Remember Grandma’s peach cobbler and fried chicken, and don’t forget about that homemade ice cream. Let’s keep all these traditions alive.</p>
+
+      <p>Our tentative schedule will include </p>
+        <ul>
+          <li><strong>Thursday</strong>: 	Meet / Greet and Registration followed by free time for local adventures and visiting.</li>
+          <li><strong>Friday</strong>:	Casino Bus Trip for those who are 21 and over to the Legendary Pechenga Casino and Knott’s Berry Farm or Disneyland for the kids and youth.</li>
+          <li><strong>Saturday</strong>:	Picnic at the Wonderful El Dorado Park followed by a trip to Universal City Walk for the evening.</li>
+          <li><strong>Sunday</strong>:	Morning Worship at the family church and culminating with a Banquet Dinner.</li>
+        </ul>
+
+      <p>Our <strong>Host Hotel and Headquarters</strong> will be the</p> 
+      
+      <p>
+        <strong>Hampton Inn Long Beach Airport</strong>:<br></br>
+        3771 N Lakewood Blvd,<br></br>
+        Long Beach, CA 90808<br></br>
+        Phone 562-425-0012</p>
+        
+      <p>This is just minutes away from Long Beach Airport. There is a large plethora of eateries which are just steps away from the Hotel. Our discounted room rate is $179 per night (2 Queens Beds or 1 King Bed and a Sofa Bed). This includes full daily breakfast for up to 5 persons per room. There is also the <strong>Homewood Suites Hotel</strong> within the same building with upgraded amenities which include: a kitchenette with separate room spaces. We would need to guarantee a 10-room minimum in order to get a discount with this brand. If you would like to do some of your own cooking the Homewood Suites is a great option.</p>
+
+      <p>It is suggested that you fly into and out of Long Beach Airport (<strong>LGB</strong>). Long Beach receives service from American, Delta, Southwest, Jet Blue, Hawaiian and Alaska Airlines. Long Beach is a much smaller airport than LAX, thus the crowds are far smaller with less traffic. The hassles of security check points are also far more convenient than those of LAX. There is a complementary shuttle provided by the Hotel to and from the airport. Other nearby airports include John Wayne (<strong>SNA</strong>)(Santa Anna) [less traffic than LAX], Los Angeles International (<strong>LAX</strong>), and Ontario International Airport (<strong>ONT</strong>).</p>  
+
+      <p>Can’t wait to see you, let’s get those wheels to turning!</p>  
+      
+      <hr></hr>
+
+      <p>July 23 – July 26, 2020</p>
+      <p>Los Angeles, California</p>
+      <p>Get ready for family fun in the sun L.A. style. We are planning a great time for all. Our tentative itinerary for this great 4-day event is as follows.</p>
+      <ul>
+        <li><strong>Thursday</strong>: 	Meet and Greet reception at hotel. This will include registration check-in, a light meal and fun and games for all.  Followed by free time for local extravaganzas at nearby attractions.</li>
+        <li><strong>Friday</strong>:	Casino Bus Trip for adults.  Amusement Park Getaway or Bowling for youth.</li>
+        <li><strong>Saturday</strong>:	Picnic at local park from 10:00 – 2:00</li>
+        <li><strong>Sunday</strong>:	Sunday Morning Worship at New Commandment Baptist Church, followed by Banquet Dinner at Hotel.  </li>
+      </ul>
+
+      <hr></hr>
+
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSchPXBvLJtD2s_nmR13GkNwBv90CQ167G0B7UexxbU3kR6euA/viewform?embedded=true" width="640" height="6349" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
   </Layout>
 )
 
