568bef62aeae2973f0e05bdb21e08e98b96146e5
Arelia <arelia@github.com>
2019-09-15 01:21:28 -0500

    add content and form

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/Users/areliajones/Documents/_repos/sam-jones-family/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-45984915-1"},
+    },{
       plugin: require('/Users/areliajones/Documents/_repos/sam-jones-family/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>July 23 – July 26, 2020</p>
+    <p>Los Angeles, California</p>
+    <p>Get ready for family fun in the sun L.A. style. We are planning a great time for all. Our tentative itinerary for this great 4-day event is as follows.</p>
+    <ul>
+      <li><strong>Thursday</strong>: 	Meet and Greet reception at hotel. This will include registration check-in, a light meal and fun and games for all.  Followed by free time for local extravaganzas at nearby attractions.</li>
+      <li><strong>Friday</strong>:	Casino Bus Trip for adults.  Amusement Park Getaway or Bowling for youth.</li>
+      <li><strong>Saturday</strong>:	Picnic at local park from 10:00 – 2:00</li>
+      <li><strong>Sunday</strong>:	Sunday Morning Worship at New Commandment Baptist Church, followed by Banquet Dinner at Hotel.  </li>
+    </ul>
+    <hr></hr>
+
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSchPXBvLJtD2s_nmR13GkNwBv90CQ167G0B7UexxbU3kR6euA/viewform?embedded=true" width="640" height="6349" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
   </Layout>
 )
 
