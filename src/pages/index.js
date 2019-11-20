a00c789edc7ee68fd0869a2ba82f88c43d35075d
Arelia <arelia@github.com>
2019-11-19 21:03:51 -0600

    update hotel info

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,25 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <center>
-      <p><strong>Thursday, July 23 – Sunday, July 26, 2020</strong></p>
+      <p><strong>Friday, July 24 – Sunday, July 26, 2020</strong></p>
       <p><strong>Los Angeles, California</strong></p>
       <p><em>Get ready for family fun in the sun L.A. style. We are planning a great time for all.</em></p>
     </center>
 
     <hr></hr>
 
-    <center><h2>Jones Family Reunion<br></br>2020 VISIONARY</h2></center>
+    <center><h2>Jones Family Reunion<br />2020 VISIONARY</h2></center>
 
-    <p>Hey family it’s time to get it popping out here. Time for the Jones Family to once again come together to celebrate Family, Unity and Strength! <strong>When: July 23 – 26, 2020</strong>. It is hard to believe that it has been nearly 2 years since the last reunion. Well this year we are meeting in the City of Angels. That’s right, we are coming to <strong>Los Angeles, California</strong>. If you have never been here, get your ticket now. Get on the bus, plane, train or in the car where all roads will lead to Los Angeles.</p>
+    <p>Hey family it’s time to get it popping out here. Time for the Jones Family to once again come together to celebrate Family, Unity and Strength!<br /><br />
+    
+    <strong>When: July 24 – 26, 2020</strong><br /><br />
+    
+    It is hard to believe that it has been nearly 2 years since the last reunion. Well this year we are meeting in the City of Angels. That’s right, we are coming to <strong>Los Angeles, California</strong>. If you have never been here, get your ticket now. Get on the bus, plane, train or in the car where all roads will lead to Los Angeles.</p>
 
     <p>This year’s reunion committee has been hard at work trying to plan and prepare for your arrival. We want to see everyone from near and far. Those cousins who live on the other coast, mid-west or wherever it is that you reside, let’s make this happen. We only do this once every 2 years.  Don’t let this opportunity go and miss spending time with your family. We love you! We will have time to spend hearing about old family legends and stories, exchanging recipes, finding out who’s who and maybe even discovering unknown talents. There are so many wonderful things to be explored within our own family. Remember Grandma’s peach cobbler and fried chicken, and don’t forget about that homemade ice cream. Let’s keep all these traditions alive.</p>
 
+
+    <h3>Schedule</h3>
     <p>Our tentative schedule will include </p>
       <ul>
         <li><strong>Thursday</strong>: 	Meet / Greet and Registration followed by free time for local adventures and visiting.</li>
@@ -28,15 +34,56 @@ const IndexPage = () => (
         <li><strong>Sunday</strong>:	Morning Worship at the family church and culminating with a Banquet Dinner.</li>
       </ul>
 
+
+    <h3>Hotel</h3>
     <p>Our <strong>Host Hotel and Headquarters</strong> will be the</p> 
     
+    <h4><a href='https://www.hilton.com/en/hotels/lgbaphx-hampton-long-beach-airport/' target='_blank' rel='noopener noreferrer'>Hampton Inn Long Beach Airport</a></h4>
+
+    <p>The Hampton Inn Long Beach Airport is just minutes away from Long Beach Airport. There is a large plethora of eateries which are just steps away from the hotel.</p>
+
     <p>
-      <strong><a href='https://www.hilton.com/en/hotels/lgbaphx-hampton-long-beach-airport/' target='_blank' rel='noopener noreferrer'>Hampton Inn Long Beach Airport</a></strong><br></br>
-      3771 N Lakewood Blvd,<br></br>
-      Long Beach, CA 90808<br></br>
-      Phone 562-425-0012</p>
-      
-    <p>This is just minutes away from Long Beach Airport. There is a large plethora of eateries which are just steps away from the hotel. Our discounted room rate is $179 per night (2 Queens Beds or 1 King Bed and a Sofa Bed). This includes full daily breakfast for up to 5 persons per room. There is also the <strong><a href='https://www.hilton.com/en/hotels/lgbarhw-homewood-suites-long-beach-airport/' target='_blank' rel='noopener noreferrer'>Homewood Suites Hotel</a></strong> within the same building with upgraded amenities which include: a kitchenette with separate room spaces. We would need to guarantee a 10-room minimum in order to get a discount with this brand. If you would like to do some of your own cooking the Homewood Suites is a great option.</p>
+      3771 N Lakewood Blvd,<br />
+      Long Beach, CA 90808<br />
+      Phone 562-425-0012
+    </p>
+
+    <p>
+      Group Name: Jones Family Reunion 2020<br />
+      Nightly Rate: $256.40 (tax and security deposit included)<br />
+      Arrival Date: 24-Jul-2020<br />
+      Departure Date: 27-Jul-2020<br />
+      Code: WJ3 <em>(Other discounts will not apply)</em><br />
+      <a href='https://secure3.hilton.com/en_US/hp/reservation/book.htm?inputModule=HOTEL&ctyhocn=LGBAPHX&spec_plan=CHHWJ3&arrival=20200724&departure=20200727&cid=OM,WW,HILTONLINK,EN,DirectLink&fromId=HILTONLINKDIRECT'>Book the Hampton Inn with Reunion Discount</a>
+    </p>
+
+    <p><strong>Rooms are limited please make your reservations ASAP. Deadline for reservations: 06/13/20. Please have a card available upon check in to cover room tax and $50 per night security deposit.</strong></p>
+
+    <hr />
+
+    <h4><a href='https://www.hilton.com/en/hotels/lgbarhw-homewood-suites-long-beach-airport/' target='_blank' rel='noopener noreferrer'>Homewood Suites by Hilton Long Beach Airport</a></h4>
+
+    <p>There is also the Homewood Suites Hotel within the same building with upgraded amenities which include a kitchenette with separate room spaces. We would need to guarantee a 10-room minimum in order to get a discount with this brand. If you would like to do some of your own cooking the Homewood Suites is a great option.</p>
+
+    <p>
+      3771 N Lakewood Blvd,<br />
+      Long Beach, CA 90808<br />
+      Phone 562-425-0010
+    </p>
+
+    <p>
+      Group Name: Jones Family Reunion 2020<br />
+      Nightly Rate: $279.40 (tax and security deposit included)<br />
+      Arrival Date: 24-Jul-2020<br />
+      Departure Date: 27-Jul-2020<br />
+      Code: WJ3 <em>(Other discounts will not apply)</em><br />
+      <a href='https://secure3.hilton.com/en_US/hw/reservation/book.htm?inputModule=HOTEL&ctyhocn=LGBARHW&spec_plan=CHWWJ3&arrival=20200724&departure=20200727&cid=OM,WW,HILTONLINK,EN,DirectLink&fromId=HILTONLINKDIRECT'>Book the Homewood Suites Hotel with Reunion Discount</a>
+    </p>
+
+    <p><strong>Rooms are limited please make your reservations ASAP. Deadline for reservations: 06/13/20. Please have a card available upon check in to cover room tax and $50 per night security deposit.</strong></p>
+
+
+    <h3>Flights to Long Beach</h3>
 
     <p>It is suggested that you fly into and out of Long Beach Airport (<strong>LGB</strong>). Long Beach receives service from American, Delta, Southwest, Jet Blue, Hawaiian and Alaska Airlines. Long Beach is a much smaller airport than LAX, thus the crowds are far smaller with less traffic. The hassles of security check points are also far more convenient than those of LAX. There is a complementary shuttle provided by the hotel to and from the airport. Other nearby airports include John Wayne (<strong>SNA</strong>)(Santa Anna) [less traffic than LAX], Los Angeles International (<strong>LAX</strong>), and Ontario International Airport (<strong>ONT</strong>).</p>  
 
